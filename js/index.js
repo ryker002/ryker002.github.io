1a89498e6fb3cc8fe385249314844170c66700da
ryker002 <ryker002@users.noreply.github.com>
2019-07-14 03:28:05 -0500

    Continued updates to gallery pages

--- js/index.js
+++ js/index.js
@@ -45,3 +45,20 @@ var $grid = $('.grid').masonry({
   // Animation Scroll
 
   new WOW().init();
+
+  // Blue Gallery
+
+function galleryObject(name) {
+  document.getElementById(name).onclick = function (event) {
+    event = event || window.event;
+    var target = event.target || event.srcElement,
+        link = target.src ? target.parentNode : target,
+        options = {index: link, event: event},
+        links = this.getElementsByTagName('a');
+    blueimp.Gallery(links, options);
+};
+}
+
+galleryObject('trevorBody');
+galleryObject('nickandpeterBody');
+galleryObject('codeyandkylieBody');
