954c4091a71d73d59057ec9a01c47e74e11043b8
ryker002 <ryker002@users.noreply.github.com>
2019-07-19 12:33:11 -0500

    babel installation

---
+++ components/card/card.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const e = React.createElement;
+
+class Card extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { liked: false };
+  }
+
+  render() {
+    return e(
+        <div className="col-md-12 col-lg-3 col-xl-2 mb-2">
+        <div className="card border-0 shadow-lg bg-c-dark text-light js-tilt js-tilt-glare wow fadeInUp" data-tilt>
+            <img className="card-img-top"
+                src={`${this.props.imageUrl}`}
+                alt={`${this.props.imageAlt}`}></img>
+            <div className="card-body">
+                <h4 className="card-title display-5 text-uppercase effra"><i
+                        className="fas fa-camera fa-md text-dark"></i> {this.props.title}</h4>
+                <p className="card-text text-light">{this.props.description}
+                    <a type="button" href={`${this.props.url}`} className="btn btn-dark ml-5 float-right">See
+                        More</a></p>
+
+            </div>
+        </div>
+    </div>
+    );
+  }
+}
+
+const domContainer = document.querySelector('#like_button_container');
+ReactDOM.render(e(Card), domContainer);
